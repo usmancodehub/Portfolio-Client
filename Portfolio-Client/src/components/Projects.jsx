64a25fd60b04b4f46577ff931de0a416979e5c33
Usman <usman.mughal0770@gmail.com>
2026-09-23 13:24:47 -0700

    Client-Potrtfolio

--- Portfolio-Client/src/components/Projects.jsx
+++ Portfolio-Client/src/components/Projects.jsx
@@ -42,9 +42,7 @@ export default function Projects() {
         </div>
 
         <div className="case-study-list">
-          {visible.map((project) => (
-            <CaseStudyCard key={project._id} project={project} />
-          ))}
+        
         </div>
       </div>
     </section>
